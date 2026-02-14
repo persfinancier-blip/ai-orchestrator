6548de7bf83c7abfd66330b45b495a9333573853
persfinancier-blip <persfinancier@gmail.com>
2026-02-15 01:21:20 +0300

    Create Tooltip.svelte

---
+++ core/orchestrator/modules/advertising/interface/desk/components/space/ui/Tooltip.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { TooltipState } from '../types';
+  export let tooltip: TooltipState;
+</script>
+
+{#if tooltip.visible}
+  <div class="tooltip" style={`left:${tooltip.x}px;top:${tooltip.y}px`}>
+    {#each tooltip.lines as line}<div>{line}</div>{/each}
+  </div>
+{/if}
