851c8dd42a937c5c14b05801e3f2a92c19616f1c
persfinancier-blip <persfinancier@gmail.com>
2026-02-15 01:20:06 +0300

    Create SaveModal.svelte

---
+++ core/orchestrator/modules/advertising/interface/desk/components/space/ui/SaveModal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let title: string;
+  export let placeholder: string;
+  export let value = '';
+  export let onCancel: () => void;
+  export let onSave: (value: string) => void;
+</script>
+
+<div class="modal-backdrop" on:click={onCancel}>
+  <div class="modal" on:click|stopPropagation>
+    <div class="modal-title">{title}</div>
+    <input class="input" placeholder={placeholder} bind:value />
+    <div class="row two">
+      <button class="btn" on:click={onCancel}>Отмена</button>
+      <button class="btn btn-primary" on:click={() => onSave(value)}>Сохранить</button>
+    </div>
+  </div>
+</div>
