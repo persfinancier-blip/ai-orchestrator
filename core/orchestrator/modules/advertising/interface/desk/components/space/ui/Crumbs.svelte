f10f94e9a1a222bc4258273a257b89fe619dcaa7
persfinancier-blip <persfinancier@gmail.com>
2026-02-15 01:21:42 +0300

    Create Crumbs.svelte

---
+++ core/orchestrator/modules/advertising/interface/desk/components/space/ui/Crumbs.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export type Crumb = { code: string; name: string };
+  export let crumbs: Crumb[] = [];
+  export let onRemove: (code: string) => void;
+</script>
+
+<div class="crumbs">
+  {#each crumbs as c (c.code)}
+    <button class="crumb" type="button" on:click={() => onRemove(c.code)}>
+      <span class="t">{c.name}</span>
+      <span class="x">×</span>
+    </button>
+  {/each}
+</div>
