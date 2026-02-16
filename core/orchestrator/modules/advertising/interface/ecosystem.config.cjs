1f69cb7f902b6ba177c63f612d391fa24105e826
persfinancier-blip <persfinancier@gmail.com>
2026-02-16 23:50:43 +0300

    Update ecosystem.config.cjs

--- core/orchestrator/modules/advertising/interface/ecosystem.config.cjs
+++ core/orchestrator/modules/advertising/interface/ecosystem.config.cjs
@@ -8,13 +8,6 @@ module.exports = {
       interpreter: 'node',
       env: {
         SPACE_API_PORT: '8787',
-
-        // Эти переменные должны быть заданы на сервере (или добавим позже в secrets и прокинем)
-        // PGHOST: '127.0.0.1',
-        // PGPORT: '5432',
-        // PGUSER: '...',
-        // PGPASSWORD: '...',
-        // PGDATABASE: '...',
       },
       max_restarts: 20,
       restart_delay: 2000,
