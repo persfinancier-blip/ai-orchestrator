9867f726fdf1c7e8bc92640f834d55f415ccae88
Антон Якушев <persfinancier@gmail.com>
2026-05-04 19:16:56 +0300

    исправил путь сборки для сервера

--- core/orchestrator/modules/advertising/interface/vite.config.js
+++ core/orchestrator/modules/advertising/interface/vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
 export default defineConfig({
-  base: '/ai-orchestrator/',
+  base: '/',
   plugins: [svelte()],
   resolve: {
     alias: {
