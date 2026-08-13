fe954e13f201a3378554f37cf5629edf5445cbf0
Антон Якушев <persfinancier@gmail.com>
2026-08-14 04:19:14 +0600

    Restore stable app entrypoint

--- core/orchestrator/modules/advertising/interface/main.js
+++ core/orchestrator/modules/advertising/interface/main.js
@@ -1,5 +1,5 @@
-import ProductApp from './product/ProductApp.svelte';
+import App from './App.svelte';
 
-const app = new ProductApp({ target: document.getElementById('app') });
+const app = new App({ target: document.getElementById('app') });
 
 export default app;
